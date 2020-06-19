716a58c2c868d9e19e257cc56fab6e5b2d380459
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-06-20 00:34:38 +0200

    lastrea unknown sketch adjustment

--- examples/lastrea_/unknown/square.js
+++ examples/lastrea_/unknown/square.js
@@ -3,5 +3,12 @@ class Square {
     this.x = x;
     this.y = y;
     this.size = s;
+    this.radius = floor(random(2, 15));
+  }
+
+  show () {
+    let c = random(20, 255);
+    fill(this.x / width * c, 0, c);
+    rect(this.x, this.y, this.size * 0.35, this.size * 0.35, this.radius);
   }
 }
