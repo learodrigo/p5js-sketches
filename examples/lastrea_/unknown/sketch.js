d7f56ee18694a1d08fc8d6ba67a098c5f0dbdcbc
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-06-20 00:37:43 +0200

    lastrea unknown sketch adjustment (#5)

--- examples/lastrea_/unknown/sketch.js
+++ examples/lastrea_/unknown/sketch.js
@@ -6,17 +6,18 @@ function setup() {
   const squares = [];
   const max_ = 200;
   const min_ = 5;
-  const n = 250;
-  const radius = 15;
-  let x, y, overlapping, s;
+  const n = 2500;
+  let x, y, s, overlapping;
 
   for (let i = 0; i < n; i++) {
     overlapping = true;
+
     while (overlapping) {
       x = random(width - min_);
       y = random(height - min_);
-      s = random(min_, min([width - x, height - y, max_]));
+      s = random(min_, min(width - x, height - y, max_));
       let square = new Square(x, y, s);
+
       overlapping = false;
 
       for (const sq of squares) {
@@ -33,8 +34,6 @@ function setup() {
   }
 
   for (const sq of squares) {
-    let c = random(20, 255);
-    fill(sq.x / width * c, 0, c);
-    rect(sq.x, sq.y, sq.size, sq.size, radius);
+    sq.show();
   }
 }
