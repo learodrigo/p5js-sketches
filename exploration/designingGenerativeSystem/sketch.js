75131db215565bb95ad254dfa98a12933925fcf2
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-10-03 16:36:04 +0200

    Return random values functions

--- exploration/designingGenerativeSystem/sketch.js
+++ exploration/designingGenerativeSystem/sketch.js
@@ -2,23 +2,6 @@ const CRYSTAL_SIZE = 500
 const SIDES = 6
 let PALETTE = []
 
-function drawBaseLines () {
-    push()
-        noFill()
-        stroke(PALETTE[0])
-        strokeWeight(5)
-        translate(width * 0.5, height * 0.5)
-        ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
-        stroke(PALETTE[1])
-        strokeWeight(1)
-        const angle = 360 / SIDES
-        for (let i = 0; i < 6; i++) {
-            line(0, 0, 0, CRYSTAL_SIZE / 2)
-            rotate(angle)
-        }
-    pop()
-}
-
 function setup () {
     createCanvas(530, 530, SVG)
 
@@ -30,6 +13,37 @@ function setup () {
     angleMode(DEGREES)
     rectMode(CENTER)
     noLoop()
+}
+
+function randomSelectTwo () {
+    return random(1) > 0.5
+}
+
+function drawBaseLines () {
+    const numShapes = randomSelectTwo() ? SIDES : SIDES * 2
+    const strokeColor = getRandomFromPalette()
+
+    push()
+        noFill()
+        stroke(PALETTE[0])
+        strokeWeight(5)
+        translate(width * 0.5, height * 0.5)
+        ellipse(0, 0, CRYSTAL_SIZE, CRYSTAL_SIZE)
+
+        const angle = 360 / numShapes
+        stroke(strokeColor)
+        strokeWeight(1)
+
+        for (let i = 0; i < numShapes; i++) {
+            line(0, 0, 0, CRYSTAL_SIZE / 2)
+            rotate(angle)
+        }
+    pop()
+}
+
+function getRandomFromPalette () {
+    const rando = floor(random(PALETTE.length))
+    return PALETTE[rando]
 }
 
 function draw () {
